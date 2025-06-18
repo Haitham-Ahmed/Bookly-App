2e32e8c644a088403c4606197f032d2a7bf699ab
Your Name <you@example.com>
2025-06-18 19:37:50 +0300

    create app styles

--- lib/Features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -16,12 +17,11 @@ class HomeViewBody extends StatelessWidget {
         children: const [
         CustomAppBar(),
          FeaturedBooksListView(),
-         const SizedBox(height: 50,),
+         SizedBox(height: 50,),
          Text(
           'Best Seller',
-          style: Styles.titleMedium,
+         style: Styles.textStyle18
          ),
-         SizedBox(height: 20,),
          BestSellerListViewItem(),
          ]),
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-// ignore: constant_identifier_names
 const KPrimaryColor = Color(0xff100B20);
 
-// ignore: constant_identifier_names
 const KTranstionDuration = Duration(milliseconds: 250);
+
+const KGtSectraFine = 'GT Sectra Fine';
