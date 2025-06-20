aa4c2db820bf59bd510a989563f5c4bea113fe40
Your Name <you@example.com>
2025-06-20 11:30:38 +0300

    add rating to book details

--- lib/Features/home/data/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly_app/core/utils/styles.dart';
@@ -32,7 +33,12 @@ class BookDetailsViewBody extends StatelessWidget {
              style: Styles.textStyle18.copyWith(
                fontWeight: FontWeight.w500,
                fontStyle: FontStyle.italic,
-             )),
+             ),
+            ),   
+          ),
+          const SizedBox(height: 18),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/Features/home/data/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/data/presentation/views/widgets/book_rating.dart
@@ -1,35 +1,33 @@
-
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({
+    super.key,
+   this.mainAxisAlignment = MainAxisAlignment.start,
+   });
 
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: mainAxisAlignment,
       children: [
-      const Icon(
-          FontAwesomeIcons.solidStar,
-          color: Color(0xffFFDD4F),
-        ),
-      const SizedBox(
-          width: 6.3,
+        const Icon(FontAwesomeIcons.solidStar,
+            size: 14,
+         color: Color(0xffFFDD4F)),
+        const SizedBox(width: 6.3),
+        const Text('4.8', style: Styles.textStyle16),
+        const SizedBox(width: 5),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '(254)',
+            style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
           ),
-      const Text(
-          '4.8',
-          style: Styles.textStyle16,
-        ),
-      const SizedBox(
-          width: 5,
-          ),
-       Text(
-          '(254)',
-          style: Styles.textStyle14
-          .copyWith(
-            color:const Color(0xff707070)),
         ),
       ],
     );
