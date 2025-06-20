caf2c36a84f559783036e0e934add8c62f3ceff6
Your Name <you@example.com>
2025-06-20 07:18:26 +0300

    create book_details_view.dart

--- lib/Features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,8 @@
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/best_seller_list_view.dart';
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
