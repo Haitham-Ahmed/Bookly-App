bd59c7ec96a7129deab1098eb43a4d3725511046
Your Name <you@example.com>
2025-06-20 23:51:12 +0300

    create custom text search field

--- lib/Features/home/data/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/data/presentation/views/widgets/custom_app_bar.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -15,10 +17,12 @@ class CustomAppBar extends StatelessWidget {
           ),
           const Spacer(),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
              icon: const Icon(
               Icons.search,
-              size: 25,
+              size: 22,
               ),
            ),      
         ],
