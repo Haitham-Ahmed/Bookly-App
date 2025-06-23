d0b33be00ab9c0435a9d157bd8ac37cc639b9db2
Your Name <you@example.com>
2025-06-23 11:25:17 +0300

    create featuerd books method

--- lib/Features/home/data/presentation/views/manger/featured_books_cubit/featuerd_books_cubit.dart
+++ lib/Features/home/data/presentation/views/manger/featured_books_cubit/featuerd_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book.model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featuerd_books_state.dart';
 
 class FeatuerdBookCubit extends Cubit<FeatuerdBookState> {
-  FeatuerdBookCubit() : super(FeatuerdBookInitial());
+  FeatuerdBookCubit(this.homeRepo) : super(FeatuerdBookInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeatuerdBookLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(FeatuerdBookFailure(failure.errMessage));
+    }, (books) {
+      emit(FeatuerdBookSuccess(books));
+    });
+  }
 }
