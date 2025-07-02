723e07a65c7100a105b37bb88ac7112c9a3bb4a0
Your Name <you@example.com>
2025-07-03 01:21:42 +0300

    refacor to use case class

--- lib/Features/home/domain/use_cases/fech_featured_books_use_cases.dart
+++ lib/Features/home/domain/use_cases/fech_featured_books_use_cases.dart
@@ -1,13 +1,18 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book.model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FechFeaturedBooksUseCases {
+class FechFeaturedBooksUseCase extends UseCase<List<Book>, NoParam> {
   final HomeRepo homeRepo;
-  FechFeaturedBooksUseCases(this.homeRepo);
-  Future<Either<Failure, List<Book>>> fetchFeaturedBooks()
-  {
-    return homeRepo.fetchFeaturedBooks();
+  FechFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<Book>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
+
+
+
