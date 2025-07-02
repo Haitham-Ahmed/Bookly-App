7bd9feb4b45548698d3beba6f2dc3de79f5a31bb
Your Name <you@example.com>
2025-07-03 00:42:20 +0300

    add Either class

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book.model.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<Book>> fetchFeaturedBooks();
-  Future<List<Book>> fetchNewsetBooks();
+  Future<Either<Failure, List<Book>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<Book>>> fetchNewsetBooks();
 }
