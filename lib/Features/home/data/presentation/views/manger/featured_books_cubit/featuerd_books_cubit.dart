720ee52cf39a9cf1a092d728b6feb3a741c8f091
Your Name <you@example.com>
2025-07-04 00:46:27 +0300

    create fetch newest book use case

--- lib/Features/home/data/presentation/views/manger/featured_books_cubit/featuerd_books_cubit.dart
+++ lib/Features/home/data/presentation/views/manger/featured_books_cubit/featuerd_books_cubit.dart
@@ -1,18 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book.model.dart';
-import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fech_featured_books_use_cases.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featuerd_books_state.dart';
 
 class FeatuerdBookCubit extends Cubit<FeatuerdBookState> {
-  FeatuerdBookCubit(this.homeRepo) : super(FeatuerdBookInitial());
+  FeatuerdBookCubit(this.featuredBooksUseCase) : super(FeatuerdBookInitial());
 
-  final HomeRepo homeRepo;
+  final FechFeaturedBooksUseCase featuredBooksUseCase;
 
   Future<void> fetchFeaturedBooks() async {
     emit(FeatuerdBookLoading());
-    var result = await homeRepo.fetchFeaturedBooks();
+    var result = await featuredBooksUseCase.call();
     result.fold((failure) {
       emit(FeatuerdBookFailure(failure.errMessage));
     }, (books) {
--- lib/Features/home/data/presentation/views/manger/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/data/presentation/views/manger/newset_books_cubit/newset_books_cubit.dart
@@ -1,18 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book.model.dart';
-import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fech_newest_books_use_case.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.fechNewestBooksUseCase) : super(NewsetBooksInitial());
 
-  final HomeRepo homeRepo;
+  final FechNewestBooksUseCase fechNewestBooksUseCase;
 
   Future<void> fetchNewsetBooks() async {
     emit(NewsetBooksLoading());
-    var result = await homeRepo.fetchNewsetBooks();
+    var result = await fechNewestBooksUseCase.call();
     result.fold(
       (failure) {
         emit(NewsetBooksFailure(failure.errMessage));
